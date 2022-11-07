93fa079fc4c772289ddedf54d98c70209f382386
Tiago Mello <tiago@cofenster.com>
2022-11-07 19:00:14 +0100

    Add telescope

--- .config/nvim/lua/timello/packer.lua
+++ .config/nvim/lua/timello/packer.lua
@@ -24,4 +24,9 @@ return require('packer').startup(function(use)
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
