ec239af6059dcb5d46000ca6aa20005adb3b2ad9
Tiago Mello <tiago@cofenster.com>
2023-01-16 10:50:57 +0100

    Add Telescome git worktree mappings

--- .config/nvim/lua/timello/remap.lua
+++ .config/nvim/lua/timello/remap.lua
@@ -12,3 +12,7 @@ nnoremap("<leader>pb", "<cmd>Buffers<CR>")
 -- Coc
 nnoremap("<leader>gd", "<Plug>(coc-definition)")
 nnoremap("<leader>gr", "<Plug>(coc-references)")
+
+-- Telescope
+nnoremap("<leader>lw", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
+nnoremap("<leader>cw", "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>")
