d2966cbc94b896480088aaad76a2f15cfa24ff38
Tiago Mello <tiago@cofenster.com>
2024-04-05 14:55:29 +0200

    Add changes for supporting rust via lspconfig
    https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/\#why-neovim-for-rust-development

--- .config/nvim/lua/timello/packer.lua
+++ .config/nvim/lua/timello/packer.lua
@@ -26,4 +26,25 @@ return require('packer').startup(function(use)
   use { 'ThePrimeagen/git-worktree.nvim' }
   use { 'voldikss/vim-floaterm' }
   use { 'Exafunction/codeium.vim' }
+
+  -- https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'simrat39/rust-tools.nvim'
+
+  -- Completion framework:
+  use 'hrsh7th/nvim-cmp'
+
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/vim-vsnip'
+
 end)
--- .config/nvim/lua/timello/set.lua
+++ .config/nvim/lua/timello/set.lua
@@ -41,5 +41,64 @@ vim.api.nvim_create_autocmd({ "DirChanged" }, {
   command = [[call chansend(v:stderr, printf("\033]7;file://%s\033\\", v:event.cwd))]],
 })
 
--- https://github.com/Exafunction/codeium.vim
--- vim.g.codeium_disable_bindings = 1
+
+--
+-- START: https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/
+--
+
+-- LSP Diagnostics Options Setup
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300)
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error
+-- Show inlay_hints more frequently
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+--
+-- END: https://rsdlt.github.io/posts/rust-nvim-ide-guide-walkthrough-development-debug/
+--
